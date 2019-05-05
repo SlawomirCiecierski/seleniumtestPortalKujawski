553ad327dc534deb6ecba7c5ccfb794559804c71
ciecierski <19051965!QAZ2wsx>
2019-05-05 18:39:33 +0200

    first commit

---
+++ src/test/java/com/slawomirciecierski/seleniumtest/CucumberRunner.java
@@ -0,0 +1,11 @@
+package com.slawomirciecierski.seleniumtest;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin={"pretty", "html:cucumber"})
+public class CucumberRunner {
+
+}
